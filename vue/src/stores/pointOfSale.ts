266ae068b5fb9a608d6bab50e4a9b8bd95257e59
GaelleBriet <gaelle.briet@protonmail.com>
2023-11-22 19:12:22 +0100

    fix

--- vue/src/stores/pointOfSale.ts
+++ vue/src/stores/pointOfSale.ts
@@ -1,20 +1,27 @@
 import { defineStore } from "pinia";
 
+export interface PointOfSaleState {
+  pointOfSaleSelected: boolean;
+  name: string;
+}
+
 export const usePointOfSaleStore = defineStore("pointOfSale", {
-  state: () => ({
-    pointOfSaleSelected: false,
-    name: "Aix-en-Provence - Parc Jourdan"
-  }),
+  state: function (): PointOfSaleState {
+    return {
+      pointOfSaleSelected: false,
+      name: "Aix-en-Provence - Parc Jourdan"
+    };
+  },
   getters: {
-    getPointOfSaleSelected() {
-      return this.pointOfSaleSelected;
+    getPointOfSaleSelected: function (state: PointOfSaleState): boolean {
+      return state.pointOfSaleSelected;
     },
-    getName() {
-      return this.name;
+    getName: function (state: PointOfSaleState): string {
+      return state.name;
     }
   },
   actions: {
-    setPointOfSaleSelected() {
+    setPointOfSaleSelected: function (this: PointOfSaleState): void {
       this.pointOfSaleSelected = true;
     }
   }
