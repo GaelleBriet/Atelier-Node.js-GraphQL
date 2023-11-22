9ffd4ea69f67430e3a9d87bb35fe066cae1c821e
GaelleBriet <gaelle.briet@protonmail.com>
2023-11-22 16:03:58 +0100

    rent forms

---
+++ vue/src/views/EndRentView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import TitleComponent from "../components/TitleComponent.vue";
+import FormText from "../components/formkit/FormText.vue";
+import FormSelect from "../components/formkit/FormSelect.vue";
+
+const router = useRouter();
+
+const currentStep = ref(1);
+const submitButtonLabel = ref("Suivant");
+
+const onSubmit = () => {
+  currentStep.value++;
+  submitButtonLabel.value = "Enregistrer";
+  if (currentStep.value === 2) {
+    //
+  }
+};
+</script>
+
+<template>
+  <div class="container ps-5">
+    <TitleComponent title="Récupérer une location" />
+    <FormKit type="form" @submit="onSubmit" :submit-label="submitButtonLabel">
+      <div class="col-6">
+        <FormText :label="'Numéro du vélo'" :placeholder="'Vélo'" />
+      </div>
+      <template v-if="currentStep === 2">
+        <div>
+          <p>Nom :<span>Hapeuprait</span></p>
+          <p>Prénom :<span>Jean-Michel</span></p>
+          <p>Loué au point de vente :<span></span></p>
+          <p>Date de retour prévue :<span>24/07/2021</span></p>
+          <FormSelect :label="'Retour au point de vente'" :options="['Aix-en-Provence - Parc Jourdan', '2', '3']" />
+        </div>
+      </template>
+    </FormKit>
+  </div>
+</template>
+
+<style>
+@import "../assets/main.css";
+</style>
---
+++ vue/src/views/StartRentView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import TitleComponent from "../components/TitleComponent.vue";
+import FormText from "../components/formkit/FormText.vue";
+import FormSelect from "../components/formkit/FormSelect.vue";
+</script>
+
+<template>
+  <div class="container ps-5">
+    <TitleComponent :title="'Démarrer une location'" />
+    <FormKit type="form" @submit="onSubmit">
+      <div class="row">
+        <div class="col-6">
+          <FormText :label="'Nom du client'" :placeholder="'Nom'" />
+        </div>
+        <div class="col-6">
+          <FormText :label="'Prénom du client'" :placeholder="'Prénom'" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <FormText :label="'Numéro du vélo'" :placeholder="'Vélo'" />
+        </div>
+        <div class="col-6">
+          <FormSelect :label="'Point de vente'" :options="['Aix-en-Provence - Parc Jourdan', '2', '3']" />
+        </div>
+      </div>
+      <FormText :label="'Date de retour prévue'" />
+    </FormKit>
+  </div>
+</template>
+
+<style>
+@import "../assets/main.css";
+</style>
