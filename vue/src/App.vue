012ab4397b836914eea5260155685acda9e17da0
GaelleBriet <gaelle.briet@protonmail.com>
2023-11-22 10:03:08 +0100

    init first pages structure

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
+import NavBarComponent from './components/NavBarComponent.vue';
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
-
+  <NavBarComponent />
+  <HomeView />
 </template>
 
 <style scoped>
