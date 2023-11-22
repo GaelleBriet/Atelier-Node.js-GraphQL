71b670f1534b4042c1582c14f31aee10289ceaf7
GaelleBriet <gaelle.briet@protonmail.com>
2023-11-22 17:08:09 +0100

    wip filtercomponent

---
+++ vue/src/components/FilterComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import FormSelect from "../components/formkit/FormSelect.vue";
+</script>
+<template>
+  <div>
+    <FormKit type="form" @submit="onSubmit" :submit-label="submitButtonLabel">
+      <FormSelect :label="'Type'" :options="['Tous', 'type1', 'type2']" />
+      <FormSelect :label="'Statut'" :options="['Tous', 'En location', 'Disponible', 'En réparatin']" />
+      <FormSelect :label="'Point de vente'" :options="['Tous', 'Aix-en-Provence - Parc Jourdan', '2', '3']" />
+    </FormKit>
+  </div>
+</template>
+<style></style>
